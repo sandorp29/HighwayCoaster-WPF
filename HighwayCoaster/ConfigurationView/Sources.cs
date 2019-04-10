134c0235e75022a6ba8d707f5abe83417a0974dc
DESKTOP-OKV6P4U\Felhasználó <puschmann2@gmail.com>
2019-04-10 15:32:08 +0200

    First Commit. MainViewModel Creation

---
+++ HighwayCoaster/ConfigurationView/Sources.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationView
+{
+    public class Sources
+    {
+        
+            public string BackgroundIMG { get { return @"Resources/Background.png"; } }
+            public string LogoIMG { get { return @"Resources/LOGO.png"; } }
+        
+    }
+}
---
+++ HighwayCoaster/HighwayCoaster/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ConfigurationView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace HighwayCoaster.ViewModels
+{
+
+    class MainViewModel
+    {
+        Sources sc = new Sources();
+
+        public ImageSource Background
+        {
+            get
+            {
+                return new BitmapImage(new Uri(sc.BackgroundIMG, UriKind.Relative));
+            }
+
+        }
+    }
+}
