e1d0ff086d4d9e71b0ef18e2a6b17cb7982cd282
DESKTOP-OKV6P4U\Felhasználó <puschmann2@gmail.com>
2019-04-12 14:15:58 +0200

    MainMenuUserControl added + gitignore

---
+++ HighwayCoaster/HighwayCoaster/ViewModels/MainMenuWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighwayCoaster.ViewModels
+{
+    class MainMenuWindowViewModel : MainViewModel
+    {
+        
+    }
+}
