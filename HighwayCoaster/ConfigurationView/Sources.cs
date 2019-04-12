82997a68fd6e111f68f6965bfd388c3f358d6908
DESKTOP-OKV6P4U\Felhasználó <puschmann2@gmail.com>
2019-04-12 13:28:26 +0200

    Sources Directory Getter

--- HighwayCoaster/ConfigurationView/Sources.cs
+++ HighwayCoaster/ConfigurationView/Sources.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,9 @@ namespace ConfigurationView
 {
     public class Sources
     {
-        
-            ////public string BackgroundIMG { get { return @"Resources/Background"; } }
-            //public string LogoIMG { get { return @"Resources/LOGO.png"; } }
-        public string BackgroundIMG { get { return AppDomain.CurrentDomain.BaseDirectory + @"\Resources\1.jpg"; } }
-        public string LogoIMG { get { return AppDomain.CurrentDomain.BaseDirectory + @"\Resources\LOGO.png"; } }
+        //public string BackgroundIMG { get { return AppDomain.CurrentDomain.BaseDirectory + @"\Resources\1.jpg"; } }
+        //public string LogoIMG { get { return AppDomain.CurrentDomain.BaseDirectory + @"\Resources\LOGO.png"; } }
+        public string LogoIMG { get { return Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName) + @"\ConfigurationView\Resources\LOGO.png"; } }
+        public string BackgroundIMG { get { return Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName) + @"\ConfigurationView\Resources\1.jpg"; } }
     }
 }
