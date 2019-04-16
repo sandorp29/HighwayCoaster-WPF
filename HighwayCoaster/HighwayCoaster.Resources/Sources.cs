7fb7c750702e9acb0d59381aa13c00268fd43a24
Sándor Papp <sandor.p29@gmail.com>
2019-04-16 12:17:15 +0200

    ConfigurationView rename to HighwayCoaster.Resources

---
+++ HighwayCoaster/HighwayCoaster.Resources/Sources.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighwayCoaster.Resources
+{
+    public class Sources
+    {
+        
+        public string LogoIMG { get { return AppDomain.CurrentDomain.BaseDirectory + @"\Resources\LOGO.png"; } }
+        public string BackgroundIMG { get { return AppDomain.CurrentDomain.BaseDirectory + @"\Resources\1.jpg"; } }
+    }
+}
