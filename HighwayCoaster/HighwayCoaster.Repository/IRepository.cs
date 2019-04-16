527195facc62ec778113b541e8af166e3ed93c66
Sándor Papp <sandor.p29@gmail.com>
2019-04-16 19:49:40 +0200

    Repository - Database creation, IRepository definition

---
+++ HighwayCoaster/HighwayCoaster.Repository/IRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace HighwayCoaster.Repository
+{
+    using System.Collections.Generic;
+
+    public interface IRepository
+    {
+        Player Login(string userName, string password);
+
+        bool Register(string userName, string password);
+
+        void DeleteHighscore(decimal playerId);
+
+        void DeleteUser(decimal playerId);
+
+        void SaveHighscore(decimal playerId, int highScore);
+
+        void ChangeCar(decimal playerId, decimal carId);
+
+        IEnumerable<Player> GetPlayers();
+
+        IEnumerable<Car> GetCars();
+    }
+}
