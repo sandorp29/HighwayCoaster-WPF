e28128defc3535814ce1447fdd6db57f368eb031
Sándor Papp <sandor.p29@gmail.com>
2019-05-11 13:13:30 +0200

    CarSelection patches

---
+++ HighwayCoaster/HighwayCoaster/Controls/ControlConverters/ImgPathConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace HighwayCoaster.Controls.ControlConverters
+{
+    class ImgPathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                ImageSource imageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + (value as string)));
+                return imageSource;
+            }
+            catch { }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
